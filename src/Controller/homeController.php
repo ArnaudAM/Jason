b972d525dd48e52592a87534a1406a0d5398edbd
ArnaudAM <arnaud.auzoudm@gmail.com>
2021-12-10 11:25:37 +0100

    Premier commit

---
+++ src/Controller/homeController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Repository\GlobalExceptionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @Route("/", name="home_")
+ */
